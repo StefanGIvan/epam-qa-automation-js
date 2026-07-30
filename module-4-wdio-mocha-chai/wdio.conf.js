6ba24ca7dc9c32aa6d88e5694167c2117858524e
Stefan Ivan <stefanivan41@gmail.com>
2026-07-30 18:10:39 +0300

    Integrate spec and HTML reporters

--- module-4-wdio-mocha-chai/wdio.conf.js
+++ module-4-wdio-mocha-chai/wdio.conf.js
@@ -123,7 +123,21 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        'spec', 
+        [
+            'html-nice',
+            {
+                outputDir: './reports/html-reports/',
+                filename: 'report.html',
+                reportTitle: 'WDIO Test Report',
+                linkScreenshots: true,
+                showInBrowser: false,
+                collapseTests: false,
+                useOnAfterCommandForScreenshot: false,
+            },
+        ],
+    ],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
@@ -145,8 +159,16 @@ export const config = {
      * @param {object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
      */
-    // onPrepare: function (config, capabilities) {
-    // },
+     /*onPrepare: function (config, capabilities) {
+        reportAggregator = new ReportGenerator({
+            outputDir: './reports/html-reports/',
+            filename: 'master-report.html',
+            reportTitle: 'WDIO Master Report',
+            collapseTests: false,
+        });
+
+        reportAggregator.clean();
+     },*/
     /**
      * Gets executed before a worker process is spawned and can be used to initialize specific service
      * for that worker as well as modify runtime environments in an async fashion.
@@ -270,8 +292,11 @@ export const config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
-    // onComplete: function(exitCode, config, capabilities, results) {
-    // },
+    /*onComplete: async function () {
+        if(reportAggregator) {
+            await reportAggregator.createReport();
+        }
+    },*/
     /**
     * Gets executed when a refresh happens.
     * @param {string} oldSessionId session ID of the old session
